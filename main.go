3f02103b5478d832b511006292b468a5af873800
zhaoting <370898269@qq.com>
2021-04-27 19:32:54 +0800

    add service registers

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net"
+	"sync"
 	"time"
 
-	"github.com/zhao2490/my-rpc/codec"
-	"github.com/zhao2490/my-rpc/config"
+	"github.com/zhao2490/my-rpc/client"
 	"github.com/zhao2490/my-rpc/server"
 )
 
@@ -28,25 +27,24 @@ func main() {
 	go startServer(addr)
 
 	// in fact, following code is like a simple my-rpc client
-	conn, _ := net.Dial("tcp", <-addr)
 	//conn, _ := os.OpenFile("./zzz.txt", os.O_CREATE|os.O_WRONLY, 0777)
-	defer func() { _ = conn.Close() }()
+	client, _ := client.Dial("tcp", <-addr)
+	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
-	// send options
-	_ = json.NewEncoder(conn).Encode(config.DefaultOption)
-	cc := codec.NewGobCodec(conn)
 	// send request & receive response
+	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
-		h := &codec.Header{
-			ServiceMethod: "Foo.Sum",
-			Seq:           uint64(i),
-		}
-		_ = cc.Write(h, fmt.Sprintf("my-rpc req %d", h.Seq))
-		_ = cc.ReadHeader(h)
-		log.Println("header:", h)
-		var reply string
-		_ = cc.ReadBody(&reply)
-		log.Println("reply:", reply)
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := fmt.Sprintf("geerpc req %d", i)
+			var reply string
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Println("reply:", reply)
+		}(i)
 	}
+	wg.Wait()
 }
