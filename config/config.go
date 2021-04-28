789fc1a60a018b0b4f9618f60585da25463c7574
zhaoting <370898269@qq.com>
2021-04-28 10:24:20 +0800

    add timeout config

--- config/config.go
+++ config/config.go
@@ -1,15 +1,21 @@
 package config
 
-import "github.com/zhao2490/my-rpc/codec"
+import (
+	"github.com/zhao2490/my-rpc/codec"
+	"time"
+)
 
 const MagicNumber = 0x3bef5c
 
 type Option struct {
-	MagicNumber int        // MagicNumber marks this's a my-rpc request
-	CodecType   codec.Type // client may choose different Codec to encode body
+	MagicNumber    int        // MagicNumber marks this's a my-rpc request
+	CodecType      codec.Type // client may choose different Codec to encode body
+	ConnectTimeout time.Duration
+	HandleTimeout  time.Duration
 }
 
 var DefaultOption = &Option{
-	MagicNumber: MagicNumber,
-	CodecType:   codec.GobType,
+	MagicNumber:    MagicNumber,
+	CodecType:      codec.GobType,
+	ConnectTimeout: time.Second * 10,
 }
